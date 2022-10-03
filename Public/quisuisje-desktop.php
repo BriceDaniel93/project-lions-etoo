e8f0158741ecedb58eb0b60c6900acd238abf56a
Jean-Marc Guilaume <jeanmarc.guillaumee@gmail.com>
2022-10-03 19:46:45 +0200

    update 03/10 JMG

--- Public/quisuisje-desktop.php
+++ Public/quisuisje-desktop.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="Style/style.css" rel="stylesheet" type="text/css">
     <link href="Style/quisuisje.css" rel="stylesheet" type="text/css">
     <!-- <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'> -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700" rel="stylesheet">
@@ -14,38 +15,199 @@
 
 <body>
 
-    <?php require_once("header.php"); ?>
-
     <main>
 
-        <!-- Section principale composé d'un grid de 4 sections-->
-        <section class="grid-main debug">
+        <section class="main-container">
 
-            <!-- 1ere section pour les palmares-->
-            <section class="grid-palmares-frame">
+            <?php require_once("header.php"); ?>
 
-                <article>
-                    <p class="onglet">PALMARES</p>
-                </article>
+            <!-- Section principale entre header et footer-->
 
+            <section class="main-frame debug">
+
+                <!-- Section principale composé d'un grid de 4 sections-->
+
+                <section class="grid2r2c h-100pct">
+
+
+                    <!-- Frame palmares -->
+
+                    <section class="w-100pct h-100pct w-center h-center debug">
+
+                        <section class="w-90pct h-90pct debug">
+
+                            <!-- Frame onglet palmares -->
+                            <section class="frame w-25pct h-10pct w-center h-center onglet">
+                                <article>
+                                    <p>PALMARES</p>
+                                </article>
+                            </section>
+
+
+                            <!-- Grid palmares -->
+                            <section class="w-100pct h-80pct bgcolor-palmares-parcours">
+
+                                <section class="grid2r3c h-100pct w-100pct">
+
+                                    <section class="w-100pct h-100pct w-center h-center">
+                                        <section class="w-75pct h-75pct img-coppaitalia"></section>
+                                    </section>
+
+                                    <section class="w-100pct h-100pct w-center h-center">
+                                        <section class="w-75pct h-75pct  img-UAE"></section>
+                                    </section>
+
+                                    <section class="w-100pct h-100pct w-center h-center">
+                                        <section class="w-75pct h-75pct  img-championL"></section>
+                                    </section>
+
+                                    <section class="w-100pct h-100pct w-center h-center">
+                                        <section class="w-75pct h-75pct img-copparey"></section>
+                                    </section>
+
+                                    <section class="w-100pct h-100pct w-center h-center">
+                                        <section class="w-75pct h-75pct  img-mali"></section>
+                                    </section>
+
+                                    <section class="w-100pct h-100pct w-center h-center">
+                                        <section class="w-75pct h-75pct  img-JO"></section>
+                                    </section>
+
+                                </section>
+
+                            </section>
+
+                            <!-- bandeau affichant les resultats -->
+                            <section class="w-100pct h-10pct bgcolor-palmares-parcours">
+
+                                <section class="bandeau-palmares">
+                                    <article>
+                                        <h1>2 Coupes d’Italie: 2009/10, 2010/11 (Inter)</h1>
+                                    </article>
+                                </section>
+
+                            </section>
+
+                        </section>
+
+                    </section>
+
+
+                    <!-- Frame Formations -->
+
+                    <section class="w-100pct h-100pct">
+
+                        <section class="w-100pct h-100pct w-center h-center">
+
+                            <section class="w-90pct h-90pct">
+
+                                <!-- Frame onglet palmares -->
+                                <section class="frame w-25pct h-10pct w-center h-center onglet">
+                                    <article>
+                                        <p>FORMATIONS</p>
+                                    </article>
+                                </section>
+
+                                <section class="w-100pct h-90pct bgcolor-formations-langues">
+                                    <section class="grid3r2c h-100pct w-100pct debug ">
+
+                                        <section class="w-100pct h-100pct w-center h-center debug">
+                                            <section class="w-75pct h-75pct img-cadets debug"></section>
+                                        </section>
+
+                                        <section class="w-100pct h-100pct w-center h-center debug">
+                                            <section class="w-90pct h-90pct h-center debug">
+                                                <article>
+                                                    <h2>Débute avec les Cadets camerounais</h2>
+                                                </article>
+                                            </section>
+                                        </section>
+
+                                        <section class="w-100pct h-100pct w-center h-center debug">
+                                            <section class="w-75pct h-75pct img-kadji debug"></section>
+                                        </section>
+
+
+                                        <section class="w-100pct h-100pct w-center h-center debug">
+                                            <section class="w-90pct h-90pct h-center debug">
+                                                <article>
+                                                    <h2>La Kadji Sport Academies - centre de formation sportive</h2>
+                                                </article>
+                                            </section>
+                                        </section>
+
+
+
+                                        <!-- <section class="w-100pct h-100pct debug"> -->
+                                        <section class="w-100pct h-100pct w-center h-center debug">
+                                            <section class="w-50pct h-100pct img-hac debug"></section>
+                                        </section>
+                                        <!-- </section> -->
+
+                                        <section class="w-100pct h-100pct w-center h-center debug">
+                                            <section class="w-90pct h-90pct h-center debug">
+                                                <article>
+                                                    <h2>Fait ses premières armes avec le footbal club du Havre</h2>
+                                                </article>
+                                            </section>
+                                        </section>
+
+
+                                        <section class="w-100pct h-100pct debug"></section>
+
+                                    </section>
+                                </section>
+
+
+                            </section>
+                        </section>
+                    </section>
+
+                    <!-- Frame Parcours -->
+                    <section class="w-100pct h-100pct debug">
+                        <section class="w-100pct h-100pct w-center h-center debug">
+
+                            <section class="w-90pct h-90pct debug">
+
+                                <!-- Frame onglet palmares -->
+                                <section class="frame w-25pct h-10pct w-center h-center onglet">
+                                    <article>
+                                        <p>PARCOURS</p>
+                                    </article>
+                                </section>
+
+
+
+                            </section>
+                        </section>
+
+                        <section class="w-100pct h-100pct debug"></section>
+
+                    </section>
+
+                </section>
+
+
+                <!-- 1ere section pour les palmares-->
+                <!-- <section class="grid-palmares-frame"> -->
                 <!-- Frame principale des palmares -->
-                <section class="frame-palmares-parcours ">
+                <!-- <section class="frame-palmares-parcours "> -->
 
-                    <!-- Grid des 6 logos  -->
-                    <section class="grid-palmares-logo">
+                <!-- Grid des 6 logos  -->
+                <!-- <section class="grid-palmares-logo">
                         <article class="grid-coppaitalia"></article>
                         <article class="grid-UAE"></article>
                         <article class="grid-championL"></article>
                         <article class="grid-copparey"></article>
                         <article class="grid-mali"></article>
                         <article class="grid-JO"></article>
-                    </section>
+                    </section> -->
 
 
-                    <section class="titre-palmares">
+                <!-- <section class="bandeau-palmares">
                         <article>
-                            <h1>2 Coupes d’Italie: 2009/10, 2010/11 (Inter)</h1>
-                            <!-- &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+                            <h1>2 Coupes d’Italie: 2009/10, 2010/11 (Inter)</h1> -->
+                <!-- &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
                             3 Champions League: 2005/06, 2008/09 (FC Barcelone), 2009/10 (Inter)
                             &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
                             3 Championnats d’Espagne: 2004/05, 2005/06, 2008/09 (FC Barcelone)
@@ -62,140 +224,148 @@
                             &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
                             Médaillé d’or aux J.O: 2000 (Cameroun)
                         </h3> -->
-                        </article>
+                <!-- </article>
                     </section>
 
-                </section>
+                </section> -->
+
+                <!-- </section>
+
+                <section class="frame w-100pct h-100pct debug"></section>
+                <section class="frame w-100pct h-100pct debug"></section>
+                <section class="frame w-100pct h-100pct debug"></section>
+ -->
+
+                <!-- Formations  -->
+                <!-- <div class="grid-main-formation">
+                    <h3 class="onglet">FORMATIONS</h3>
+
+
+                    <div class="frame-formations-langues">
+
+
+                        <div class="grid-formations">
+
+                            <div class="grid-formations-cadets-logo"></div>
+                            <div class="grid-formations-cadets-text">
+                                <h2>Débute avec les Cadets camerounais</h2>
+                            </div>
+
+                            <div class="grid-formations-kad-logo"></div>
+
+                            <div class="grid-formations-kad-text">
+                                <h2>La Kadji Sport Academies - centre de formation sportive</h2>
+                            </div>
+
+                            <div class="grid-formations-hac-logo"></div>
+
+                            <div class="grid-formations-hac-text">
+                                <h2>Fait ses premières armes avec le footbal club du Havre</h2>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                </div> -->
+
+                <!-- Parcours  -->
+                <!-- <div class="grid-main-parcours">
+                    <h3 class="onglet">PARCOURS</h3>
+
+                    <div class="frame-palmares-parcours">
+
+                        <div class="grid-parcours">
+                            <div class="grid-img-parcours-intermilan"></div>
+                            <div class="grid-img-parcours-fcb"></div>
+                            <div class="grid-img-parcours-rcd"></div>
+
+                            <div class="grid-parcours-club">
+                                <h1>INTER MILAN</h1>
+                                <h2>DIVISION A</h2>
+                                <h2>2011 - 2009</h3>
+                            </div>
+
+                            <div class="grid-parcours-club">
+                                <h1>FCB</h1>
+                                <h2>DIVISION 1</h2>
+                                <h2>2009 - 2004</h3>
+                            </div>
+
+                            <div class="grid-parcours-club">
+                                <h1>RCB</h1>
+                                <h2>DIVISION 1</h2>
+                                <h2>2004 - 1999</h3>
+                            </div>
+
+                            <div class="grid-parcours-buts">
+                                <div class="grid-parcours-buts-ballon"></div>
+                                <h2>77 buts</h2>
+                            </div>
+
+                            <div class="grid-parcours-buts">
+                                <div class="grid-parcours-buts-ballon"></div>
+                                <h2>37 buts</h2>
+                            </div>
+
+                            <div class="grid-parcours-buts">
+                                <div class="grid-parcours-buts-ballon"></div>
+                                <h2>33 buts</h2>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+ -->
+                <!-- Langes  -->
+                <!-- <div class="grid-main-langues debug">
+                    <h3 class="onglet">LANGUES</h3>
+
+                    <div class="frame-formations-langues">
+                        <div class="grid-langues">
+
+                            <div class="container-center debug1">
+                                <div class="grid-langue-francais-logo"></div>
+                            </div>
+
+                            <div class="container-center">
+                                <div class="grid-langue-anglais-logo"></div>
+                            </div>
+
+                            <div class="container-center">
+                                <div class="grid-langue-espagnol-logo"></div>
+                            </div>
+
+                            <div class="container-center">
+                                <h1>FRANCAIS</h1>
+                            </div>
+
+                            <div class="container-center">
+                                <h1>ANGLAIS</h1>
+                            </div>
+
+                            <div class="container-center">
+                                <h1>ESPAGNOL</h1>
+                            </div>
+
+
+                        </div>
+                    </div>
+
+                </div> -->
+
+                <!-- </section> -->
+                <!-- <section class="frame w-100pct h-10pct"> -->
+
+                <?php require_once("footer.php"); ?>
+
 
             </section>
 
-
-            <!-- Formations  -->
-            <div class="grid-main-formation">
-                <h3 class="onglet">FORMATIONS</h3>
-
-
-                <div class="frame-formations-langues">
-
-
-                    <div class="grid-formations">
-
-                        <div class="grid-formations-cadets-logo"></div>
-                        <div class="grid-formations-cadets-text">
-                            <h2>Débute avec les Cadets camerounais</h2>
-                        </div>
-
-                        <div class="grid-formations-kad-logo"></div>
-
-                        <div class="grid-formations-kad-text">
-                            <h2>La Kadji Sport Academies - centre de formation sportive</h2>
-                        </div>
-
-                        <div class="grid-formations-hac-logo"></div>
-
-                        <div class="grid-formations-hac-text">
-                            <h2>Fait ses premières armes avec le footbal club du Havre</h2>
-                        </div>
-
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <!-- Parcours  -->
-            <div class="grid-main-parcours">
-                <h3 class="onglet">PARCOURS</h3>
-
-                <div class="frame-palmares-parcours">
-
-                    <div class="grid-parcours">
-                        <div class="grid-img-parcours-intermilan"></div>
-                        <div class="grid-img-parcours-fcb"></div>
-                        <div class="grid-img-parcours-rcd"></div>
-
-                        <div class="grid-parcours-club">
-                            <h1>INTER MILAN</h1>
-                            <h2>DIVISION A</h2>
-                            <h2>2011 - 2009</h3>
-                        </div>
-
-                        <div class="grid-parcours-club">
-                            <h1>FCB</h1>
-                            <h2>DIVISION 1</h2>
-                            <h2>2009 - 2004</h3>
-                        </div>
-
-                        <div class="grid-parcours-club">
-                            <h1>RCB</h1>
-                            <h2>DIVISION 1</h2>
-                            <h2>2004 - 1999</h3>
-                        </div>
-
-                        <div class="grid-parcours-buts">
-                            <div class="grid-parcours-buts-ballon"></div>
-                            <h2>77 buts</h2>
-                        </div>
-
-                        <div class="grid-parcours-buts">
-                            <div class="grid-parcours-buts-ballon"></div>
-                            <h2>37 buts</h2>
-                        </div>
-
-                        <div class="grid-parcours-buts">
-                            <div class="grid-parcours-buts-ballon"></div>
-                            <h2>33 buts</h2>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <!-- Langes  -->
-            <div class="grid-main-langues debug">
-                <h3 class="onglet">LANGUES</h3>
-
-                <div class="frame-formations-langues">
-                    <div class="grid-langues">
-
-                        <div class="container-center debug1">
-                            <div class="grid-langue-francais-logo"></div>
-                        </div>
-
-                        <div class="container-center">
-                            <div class="grid-langue-anglais-logo"></div>
-                        </div>
-
-                        <div class="container-center">
-                            <div class="grid-langue-espagnol-logo"></div>
-                        </div>
-
-                        <div class="container-center">
-                            <h1>FRANCAIS</h1>
-                        </div>
-
-                        <div class="container-center">
-                            <h1>ANGLAIS</h1>
-                        </div>
-
-                        <div class="container-center">
-                            <h1>ESPAGNOL</h1>
-                        </div>
-
-
-                    </div>
-                </div>
-
-            </div>
-
-        </section>
-
     </main>
-
-    <?php require_once("footer.php"); ?>
 
 </body>
 
