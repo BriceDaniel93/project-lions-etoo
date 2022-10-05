567f60c9e396b94dea5517ee98e00c408e2ec87f
Cedric Nkene <cedric.nkn@gmail.com>
2022-10-05 16:11:02 +0200

    header avec lien pour naviger sur le site

--- Public/header.php
+++ Public/header.php
@@ -1,2 +1,21 @@
-<header>
-</header>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Style/header.css" />
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    <img src="../Assets/fecafoot.png" alt="Logo Fecafoot">
+            <nav>
+                 <ul>
+                     <li><a class="boutton" href="index.php">Accueil</a></li>
+                     <li><a href="quisuisje.php">Parcours & formations</a></li>
+                 </ul>
+            </nav>
+    </header>
+</body>
+</html>
