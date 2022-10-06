5fcf0c83f851520f846f1f1b54d35f45d9c79a7f
Jean-Marc Guilaume <jeanmarc.guillaumee@gmail.com>
2022-10-06 11:50:43 +0200

    update

--- Public/quisuisje.php
+++ Public/quisuisje.php
@@ -1,4 +1,7 @@
+<?php require_once("src/function.php"); ?>
+
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
@@ -82,21 +85,8 @@
 
                                     <section class="bandeau-palmares">
                                         <article>
-                                            <?php
-                                            $desc = "1 Mondial des clubs: 2010 (Inter)";
-                                            $desc .= " - 1 Coupe Intercontinentale: 1998 (Real Madrid)";
-                                            $desc .= " - 3 Champions League: 2005/06, 2008/09 (FC Barcelone), 2009/10 (Inter)";
-                                            $desc .= " - 1 Scudetto: 2009/10 (Inter)";
-                                            $desc .= " - 3 Championnats d’Espagne: 2004/05, 2005/06, 2008/09 (FC Barcelone)";
-                                            $desc .= " - 2 Coupes d’Italie: 2009/10, 2010/11 (Inter)";
-                                            $desc .= " - 2 Coupes d’Espagne: 2002/03 (Mallorca) 2008/09 (FC Barcelone)";
-                                            $desc .= " - 1 Supercoupe d’Italie: 2010";
-                                            $desc .= " - 2 Supercoupes d’Espagne: 2004/05, 2005/06 (FC Barcelone)";
-                                            $desc .= " - 2  CAN: 2000, 2002 (Cameroun)";
-                                            $desc .= " - Médaillé d’or aux J.O: 2000 (Cameroun)";
 
-                                            ?>
-                                            <h1><?php echo $desc ?></h1>
+                                            <h1><?= palmares(); ?></h1>
 
                                         </article>
                                     </section>
