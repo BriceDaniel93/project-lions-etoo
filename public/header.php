7459036637210f597ef485ca2ce8bf5e221e866d
Jean-Marc Guilaume <jeanmarc.guillaumee@gmail.com>
2022-10-10 21:10:08 +0200

    correction

---
+++ public/header.php
@@ -0,0 +1,9 @@
+<header>
+    <img src="assets/fecafoot.png" alt="Logo Fecafoot">
+    <nav>
+        <ul>
+            <li><a class="boutton" href="index.php">Accueil</a></li>
+            <li><a href="parcours_formations.php">Parcours & formations</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ public/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel='stylesheet' media='screen and (max-width: 900px)' href='style/mobile.css' />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700" rel="stylesheet">
+
+
+    <link rel="icon" href="assets/Fecafoot.jpg">
+    <title>@etoo.cv</title>
+</head>
+
+<body>
+
+    <?php
+    include('header.php');
+    // include('menu.php');
+    ?>
+
+    <div class="contenu">
+
+        <!-- <section class="hero"> -->
+        <h2>“Aujourd’hui président de la Fecafoot,
+            j’ai longtemps été footballeur professionel.
+            Père et mari attentionné,
+            je suis ce petit fan d’art et de cinéma qui
+            s’occupe d'enfants défavorisés de
+            l’Afrique .”
+        </h2>
+        <!-- </section> -->
+
+
+    </div>
+
+    <?php include('footer.php'); ?>
+</body>
